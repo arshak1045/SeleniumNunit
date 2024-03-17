7a32fdb69b55b67dc50183b598db77c3879a7f1e
arshak1045 <hayryan1045@gmail.com>
2024-03-17 15:34:58 +0400

    Initial commit.

---
+++ SeleniumNunit/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SeleniumNunit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
